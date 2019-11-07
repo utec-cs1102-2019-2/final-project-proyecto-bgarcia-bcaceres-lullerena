079ba17b4717b7a72032611f7f8fb8cf8815fe12
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-07 10:17:53 -0500

    Create Motocicleta.cpp

---
+++ Motocicleta.cpp
@@ -0,0 +1,35 @@
+
+#include "Motocicleta.h"
+
+#include <iostream>
+using namespace std;
+#include <ctime>
+Motocicleta::Motocicleta():Vehiculo(){
+    srand(time(nullptr));
+    ruedas=4;
+    peso=Min+(rand()%(Max-Min+1));
+    peso=Min+(rand()%(Max-Min+1));
+    chasis=Min+(rand()%(Max-Min+1));
+    carroceria=Min+(rand()%(Max-Min+1));
+    x=0;
+    y=0;
+    VelMin=0;
+    VelMax=800;
+    VelActual=VelMin+(rand()%(VelMax-VelMin+1));
+}
+
+void Motocicleta::avanzar(){
+    cout<<"Bicileta avanzando ";
+    cout<<"desde "<<x<<" , "<<y<<endl;
+    x=x+VelActual;
+    cout<<"Hasta "<<x<<" , "<<y<<endl;
+}
+void Motocicleta::retroceder(){
+    x=x-VelActual;
+}
+void Motocicleta::mejorar(){
+    ruedas=ruedas*efecto;
+    chasis=chasis*efecto;
+    carroceria=chasis*efecto;
+    zapatillas=zapatillas*efecto;
+}
