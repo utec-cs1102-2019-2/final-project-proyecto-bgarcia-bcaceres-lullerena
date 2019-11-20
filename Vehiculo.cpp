972ef2d5d09005e9ad6ece6d5dadac41883a2ec5
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-11-20 16:09:13 -0500

    Update Vehiculo.cpp

--- Vehiculo.cpp
+++ Vehiculo.cpp
@@ -1,4 +1,8 @@
 #include "Vehiculo.h"
-Vehiculo::Vehiculo() {
+Vehiculo::Vehiculo() {}
 
+Vehiculo::~Vehiculo(){}
+void Vehiculo::setnombre(string pNombre) { this->nombre = pNombre; }
+string Vehiculo::getnombre() { return nombre; }
+Vehiculo::Vehiculo(string pNombre):nombre{pNombre} {
 }
