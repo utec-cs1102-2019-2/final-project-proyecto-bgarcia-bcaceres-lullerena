6adc5aed0c3341e9cb7ff83f0b42d8cb4e7c2f71
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-11-29 15:04:36 -0500

    Create Velocidad.cpp

---
+++ Velocidad.cpp
@@ -0,0 +1,29 @@
+#include "Velocidad.h"
+#include <cstdlib>
+#include <ctime>
+Velocidad::Velocidad(int x, string personalidad) {
+    if (personalidad=="precavido"){
+        velo_int=efect_pre*vel_reco;
+    } else if(personalidad=="osado"){
+        velo_int=efect_os*vel_reco;
+}
+void Velocidad::hipotermia(int hipo, int contextura, int pista) {
+    if (pista==1){
+        hipo=rand()%((contextura-psh)-0);
+    }
+}
+void Velocidad::dificultad(piloto equilibrio, Vehiculo ruedas, string personalidad) {
+    srand(time(nullptr));
+    if (personalidad="osado"&& ruedas=2) {
+        dif = rand() % (equilibrio - (75 / peso));
+    }
+    else if (personalidad="precavido" && ruedas=2){
+        dif = rand() % ((equilibrio*25)-(75/peso));
+    }
+    else if (ruedas=0){
+        dif=0;
+    }
+}
+void Velocidad::velo_max() {
+    velo_real=vel_min + rand ()% ((velo_int+dif+hipo)-vel_min);
+}
