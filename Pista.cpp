0212ce9af06d7ac5449f9ad141daeff7c1c0840e
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-12-02 13:20:31 -0500

    Update Pista.cpp

--- Pista.cpp
+++ Pista.cpp
@@ -1,16 +1,19 @@
 #include "Pista.h"
 #include <ctime>
 #include <cstdlib>
-Pista::Pista(int pista) {
+Pista::Pista(Menu *pista) {
+    this-> pista=pista;
+}
+void Pista::tPista(int pista) {
     srand(time(nullptr));
     if (pista==1){
-    psh = 65 + rand() % (70 - 65);
+        psh = 65 + rand() % (70 - 65);
     }
     else if (pista==2){
-    psr=70+ rand()%(78-70);
+        psr=70+ rand()%(78-70);
     }
     else if (pista==3){
-    pss=78+ rand()%(85-78);
+        pss=78+ rand()%(85-78);
     }
 }
 Pista::~Pista() {}
