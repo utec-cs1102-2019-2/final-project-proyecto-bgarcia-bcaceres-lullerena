e0e5bbb6c19b1c144c5ff594c31573fd868fad75
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-12-02 15:03:29 -0500

    Update Menu.h

--- Menu.h
+++ Menu.h
@@ -2,14 +2,19 @@
 #define PROYECTO_MENU_H
 #include "Vehiculo.h"
 #include "Pista.h"
+#include "Piloto.h"
+#include "MenuVehiculos.h"
 class Menu {
     int dinero;
-    Vehiculo* m_vehiculo;
-    Pista *pista;
     int m_opcion;
     int dganado;
+    MenuVehiculos *m_xd=new MenuVehiculos();
+
 public:
+    Vehiculo* m_vehiculo;
+    piloto *Piloto;
     Menu();
+    Pista *pista;
     void opcionesV(int dinero, int dganado);
     void opcionesPistas();
     void imprimirMenu();
