a0156194d1b9b219c02a84be18dbd6a5885c2902
BrayanGaMo12 <55211796+BrayanGaMo12@users.noreply.github.com>
2019-12-02 14:03:05 -0500

    Update Velocidad.cpp

--- Velocidad.cpp
+++ Velocidad.cpp
@@ -1,21 +1,19 @@
-#include "Velocidad.h"
 #include <cstdlib>
 #include <ctime>
+#include "Velocidad.h"
 Velocidad::Velocidad(int x, string personalidad) {
     if (personalidad=="precavido"){
         velo_int=efect_pre*vel_reco;
-    } else if(personalidad=="osado"){
-        velo_int=efect_os*vel_reco;
+    } else if(personalidad=="osado") {
+        velo_int = efect_os * vel_reco;
+    }
 }
-void Velocidad::hipotermia(int hipo, int contextura, int pista) {
-    if (pista==1){
+void Velocidad::hipotermia(int contextura, int psh){
         hipo=rand()%((contextura-psh)-0);
-    } else {
-        hipo=0;
 }
-void Velocidad::dificultad(piloto equilibrio, Vehiculo ruedas, string personalidad) {
+void Velocidad::Dificultad(int equilibrio, int ruedas, string personalidad){
     srand(time(nullptr));
-    if (personalidad="osado"&& ruedas=2) {
+    if (personalidad="osado"&& ruedas==2) {
         dif = rand() % (equilibrio - (75 / peso));
     }
     else if (personalidad="precavido" && ruedas=2){
@@ -28,12 +26,3 @@ void Velocidad::dificultad(piloto equilibrio, Vehiculo ruedas, string personalid
 void Velocidad::velo_max() {
     velo_real=vel_min + rand ()% ((velo_int+dif+hipo)-vel_min);
 }
-void Velocidad::get_roca(){
-    return psr;
-}
-void Velocidad::get_helada()
-{
-    return psh;}
-void Velocidad::get_supremo(){
-    return pss;}
-
